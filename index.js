c9e67e03ab63c166bb8a78c76779da65be54aac0
rizen1892 <rizen1892@gmail.com>
2016-03-14 03:24:14 +0000

    Update 10

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-var express = require('express')
-var app = express()
+//var express = require('express')
+//var app = express()
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+//app.set('port', (process.env.PORT || 5000))
+//app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.sendfile('bitstarter.html')
-})
+//app.get('/', function(request, response) {
+//  response.sendfile('bitstarter.html')
+//})
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+//app.listen(app.get('port'), function() {
+//  console.log("Node app is running at localhost:" + app.get('port'))
+//})
+
+var http = require ('http');
+var fs = require('fs');
+
+http.createServer(function(request,response){
+	var url = request.url;
+		if(url == '/'){
+			getStaticFileContent(response, 'bitstarter.html','text/html');
+		}
+		else{
+		reponse.writeHead(404, {'Content-Type':'text/plain'});
+		response.end('Page not found.')
+		}
+	
+}.listen(5000)
+
+function getStaticFileContent(response, filepath, contentType){
+	fs.readFile(filepath, function(error, data){
+		if(error){
+			response.writeHead(500, {'Content-Type':'text/plain'});
+			response.end('500 - Internal Server Error.');
+		}
+		if (data){
+			response.writeHead(200, {'Content-Type':'text/html'});
+			response.end(data);
+		}
+	});
+}	

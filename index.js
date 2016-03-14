29d1853a3e09ae9d6f59d999e1bb9fd3701fcdee
rizen1892 <rizen1892@gmail.com>
2016-03-14 00:39:49 +0000

    Update 7

--- index.js
+++ index.js
@@ -24,5 +24,6 @@ fs.readFile('./bitstarter.html', function (err, html) {
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
-    }).listen(8000);
+    }).listen(5000);
 });
+console.log('Server is running.')

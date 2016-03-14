ec00c20278c13644a3b5f43aa93caba6bd132b99
rizen1892 <rizen1892@gmail.com>
2016-03-14 00:19:30 +0000

    Update 6

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-var express = require('express')
-var app = express()
+//var express = require('express')
+//var app = express()
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+//app.set('port', (process.env.PORT || 5000))
+//app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-response.sendfile('bitstarter.html') 
+//app.get('/', function(request, response) {
+//response.sendfile('bitstarter.html') 
+//});
+
+//app.listen(app.get('port'), function() {
+//  console.log("Node app is running at localhost:" + app.get('port'))
+//})
+
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./bitstarter.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-

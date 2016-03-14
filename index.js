9cc7e5f4e76983dbec5bd95e26bef9809bd2a26e
rizen1892 <rizen1892@gmail.com>
2016-03-14 01:07:59 +0000

    Update 9

--- index.js
+++ index.js
@@ -13,51 +13,37 @@
 //})
 
 
-var http = require("http");
-var url = require('url');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var fs = require('fs');
+var path = require('path');
 
-var server = http.createServer(function(request, response){
-    console.log('Connection');
-    var path = url.parse(request.url).pathname;
+var filePath = path.join(__dirname, 'bitstarter.html');
 
-    switch(path){
-        case '/':
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write('hello world');
-            response.end();
-            break;
-        case '/bitstarter.html':
-            fs.readFile(/home/ubuntu/CMPE172/HW2/bitstarter.html, function(error,data){
-                console.log("error: " + error);
-                console.log("data: " + data);
-                if (error){
-                    response.writeHead(404);
-                    response.write("oops problem loading this page - 404");
-                }
-                else{
-                    response.writeHead(200, {"Content-Type": "text/html"});
-                    response.write(data);
-                }
-
-                response.end();
-            });
-            break;
-        default:
-            response.writeHead(404);
-            response.write("oops this page doesn't exist - 404");
-            response.end();
-            break;
+fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (!err){
+    console.log('received data: ' + data);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+    }else{
+        console.log(err);
     }
+
 });
-
-
-
-      
-
-
-
-
 
 
 

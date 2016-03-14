05d852101c8fad627e4b7464343678109820e79b
rizen1892 <rizen1892@gmail.com>
2016-03-14 02:46:56 +0000

    Update

--- index.js
+++ index.js
@@ -1,49 +1,14 @@
-//var express = require('express')
-//var app = express()
-
-//app.set('port', (process.env.PORT || 5000))
-//app.use(express.static(__dirname + '/public'))
-
-//app.get('/', function(request, response) {
-//response.sendfile('bitstarter.html') 
-//});
-
-//app.listen(app.get('port'), function() {
-//  console.log("Node app is running at localhost:" + app.get('port'))
-//})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var http = require('http'),
 var fs = require('fs');
-var path = require('path');
 
-var filePath = path.join(__dirname, 'bitstarter.html');
 
-fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-    if (!err){
-    console.log('received data: ' + data);
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(data);
-    response.end();
-    }else{
-        console.log(err);
-    }
-
+fs.readFile('./bitstarter.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
-
-
-
